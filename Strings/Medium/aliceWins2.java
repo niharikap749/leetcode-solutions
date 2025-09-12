cfe2dae1c5b46ecf9bf878eee10467a1b69e099d
niharikap749 <niharikap749@gmail.com>
2025-09-12 22:01:32 +0530

    AliceWins @nd Solution

---
+++ Strings/Medium/aliceWins2.java
@@ -0,0 +1,12 @@
+class aliceWins2 {
+    public boolean doesAliceWin(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' ||s.charAt(i)=='o' ||s.charAt(i)=='u') {
+                count++;
+            }
+        }
+        if(count==0) return false;
+        return true;
+    }
+}
