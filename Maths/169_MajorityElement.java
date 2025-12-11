a53277b36ef1d52521fd8df01b45f768990d7043
niharikap749 <niharikap749@gmail.com>
2025-12-11 20:19:38 +0530

    New Solution

--- Maths/169_MajorityElement.java
+++ Maths/169_MajorityElement.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+    public int majorityElementSolution1(int[] nums) {
         int candidate = nums[0];
         int count = 0;
         for(int num:nums) {
@@ -18,6 +18,19 @@ class Solution {
             if(candidate==num) count++;
         }
         if(count>nums.length/2) return candidate;
+        return -1;
+    }
+
+    public int majorityElementSolution2(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums) {
+            if(map.containsKey(num)) map.put(num,map.getOrDefault(num,0)+1);
+            else map.put(num,1);
+        }
+        for(int num:nums) {
+            if(map.get(num)>n/2) return num;
+        }
         return -1;
     }
 }
