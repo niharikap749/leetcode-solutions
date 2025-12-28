ee02b3fc5b0857103e93e67aa44eaa8850042d69
niharikap749 <niharikap749@gmail.com>
2025-12-29 00:12:27 +0530

    New Solution

--- Arrays/Easy/1351_CountNegativeNumbersinaSortedMatrix.java
+++ Arrays/Easy/1351_CountNegativeNumbersinaSortedMatrix.java
@@ -1,4 +1,5 @@
-class Solution {
+//Solution 1
+class Solution1 {
     public int countNegatives(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -13,6 +14,32 @@ class Solution {
             else {
                 row++;
             }
+        }
+        return count;
+    }
+}
+
+//Solution 2
+class Solution2 {
+    public int countNegatives(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for(int i=0;i<m;i++) {
+            int left = 0;
+            int right = n-1;
+            int firstNeg = n;
+            while(left<=right) {
+                int mid = left+(right-left)/2;
+                if(grid[i][mid]<0) {
+                    firstNeg = mid;
+                    right = mid-1;
+                }
+                else {
+                    left = mid+1;
+                }
+            }
+            count+=(n-firstNeg);
         }
         return count;
     }
