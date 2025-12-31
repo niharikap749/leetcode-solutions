e0e52d318af21d88656b3423c9a957bbd592298a
niharikap749 <niharikap749@gmail.com>
2025-12-31 19:23:39 +0530

    New Solution

---
+++ Arrays/Hard/1970_LastDayWhereYouCanStillCross.java
@@ -0,0 +1,58 @@
+class Solution {
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    public boolean canCross(int row, int col, int[][] cells, int day) {
+        int[][] grid = new int[row][col];
+        for(int i=0;i<day;i++) {
+            int r = cells[i][0]-1;
+            int c = cells[i][1]-1;
+            grid[r][c] = 1;
+        }
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[row][col];
+
+        for(int j=0;j<col;j++) {
+            if(grid[0][j]==0) {
+                q.offer(new int[]{0,j});
+                visited[0][j] = true;
+            }
+        }
+
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            if(r==row-1) return true;
+
+            for(int[] d:directions) {
+                int nr = r+d[0];
+                int nc = c+d[1];
+
+                if(nr>=0 && nr<row 
+                && nc>=0 && nc<col 
+                && !visited[nr][nc] 
+                && grid[nr][nc]==0) {
+                    q.offer(new int[]{nr, nc});
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+        return false;
+    }
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        int left = 0;
+        int right = cells.length;
+        int ans = 0;
+        while(left<=right) {
+            int mid = left + (right-left)/2;
+            if(canCross(row, col, cells, mid)) {
+                ans = mid;
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+}
