d7a92ba6848a5e5bba847039b4fc8984a4936d36
niharikap749 <niharikap749@gmail.com>
2026-02-23 21:16:17 +0530

    New Solution

---
+++ Maths/868_BinaryGap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1;
+        int maxDistance = 0;
+        int pos = 0;
+        while(n>0) {
+            if((n&1)==1) {
+                if(last!=-1) {
+                    maxDistance = Math.max(maxDistance, pos-last);
+                }
+                last = pos;
+            }
+            n = n>>1;
+            pos++;
+        }  
+        return maxDistance;
+    }
+}
